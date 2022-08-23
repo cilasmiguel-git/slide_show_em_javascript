d8eadd5652a2b11574cfd446c7dd49907b023e89
unknown <email>
2022-08-23 17:36:34 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,36 @@
+let totalSliders = document.querySelectorAll('.slider--item').length;
+let currentSlide = 0;
+
+let sliderWidth = document.querySelector('.slider').clientWidth;
+
+document.querySelector('.slider--width').style.width = `${sliderWidth * totalSliders}px`;
+document.querySelector('.slider--controls').style.width = `${sliderWidth}px` ;
+document.querySelector('.slider--controls').style.height = `${document.querySelector('.slider').clientHeight}px` ;
+
+function goPrev()
+{
+   currentSlide--;
+   if(currentSlide < 0)
+   {
+       currentSlide = totalSliders - 1 ;
+   }
+    updateMargin();
+}
+function goNext()
+{
+    currentSlide++;
+    if(currentSlide > (totalSliders-1))
+    {
+        currentSlide = 0 ;
+    }
+    updateMargin();
+}
+
+function updateMargin()
+{
+    let sliderItemWidth =  document.querySelector('.slider--item').clientWidth;
+    let newMargin = (currentSlide*sliderItemWidth);
+    document.querySelector('.slider--width').style.marginLeft = `-${newMargin}px`;
+
+}
+setInterval(goNext , 5000);
